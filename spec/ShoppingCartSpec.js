b99eef8ce7a66a3a50d435c62d1e9e1982e76537
NaxYo <nax@oxik.net>
2014-04-06 19:53:29 +0200

    Refactoring

--- spec/ShoppingCartSpec.js
+++ spec/ShoppingCartSpec.js
@@ -1,5 +1,6 @@
 describe('ShoppingCart', function() {
 	var cart;
+	var singleBookPrice = 8;
 
 	beforeEach(function() {
 		cart = new ShoppingCart
@@ -17,7 +18,7 @@ describe('ShoppingCart', function() {
 		it('should be able to calculate the price of a single book', function() {
 			cart.addBook(1);
 			var price = cart.checkout();
-			expect(price).toEqual(8);
+			expect(price).toEqual(singleBookPrice);
 		});
 
 		it('should be able to calculate the price of N same books', function() {
@@ -26,13 +27,13 @@ describe('ShoppingCart', function() {
 				cart.addBook(1);
 
 			var price = cart.checkout();
-			expect(price).toEqual(n*8);
+			expect(price).toEqual(n*singleBookPrice);
 		});
 
 		it('should be able to apply discount to two different books in serie', function() {
 			var numberOfBooks = 2;
 			var discount      = .85;
-			var expectedPrice = (numberOfBooks*8)*discount;
+			var expectedPrice = (numberOfBooks*singleBookPrice)*discount;
 
 			cart.addBook(1);
 			cart.addBook(2);
--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,20 +1,19 @@
 function ShoppingCart() {
 	this.books = [];
-	this.numberOfBooks = 0;
 }
 
 ShoppingCart.prototype.addBook = function(bookID) {
 	this.books.push(bookID);
-	this.numberOfBooks++;
 }
 
 ShoppingCart.prototype.getNumberOfItems = function() {
-	return this.numberOfBooks;
+	return this.books.length;
 }
 
 ShoppingCart.prototype.checkout = function() {
-	var price = this.numberOfBooks*8;
+	var price   = this.books.length*8;
 	var isSerie = false;
+	
 	for(var i=this.books.length-1; i>0; i--)
 		if(this.books[i]-1 == this.books[i-1])
 			isSerie = true;
