806be75fd026db18ebe40d0fc58f0b265f520804
NaxYo <nax@oxik.net>
2014-04-06 18:54:29 +0200

    Refactoring

--- spec/ShoppingCartSpec.js
+++ spec/ShoppingCartSpec.js
@@ -1,14 +1,17 @@
 describe('ShoppingCart', function() {
+	var cart;
+
+	beforeEach(function() {
+		cart = new ShoppingCart
+	});
+
 	it('should allow us to add a book', function() {
-		var cart = new ShoppingCart;
 		cart.addBook(1);
 		expect(cart.getNumberOfItems()).toEqual(1);
 	});
 
 	it('should allow us to add N books', function() {
-		var cart = new ShoppingCart;
-		var n    = 10;
-
+		var n = 10;
 		for(var i = 0; i<n; i++)
 			cart.addBook(1);
 
