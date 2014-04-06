5692ae2c09a80736581687e9ff0d72cba46bb01b
NaxYo <nax@oxik.net>
2014-04-06 18:46:02 +0200

    Failing test: add a book and count

--- spec/ShoppingCartSpec.js
+++ spec/ShoppingCartSpec.js
@@ -2,5 +2,6 @@ describe('ShoppingCart', function() {
 	it('should allow us to add a book', function() {
 		var cart = new ShoppingCart;
 		cart.addBook(1);
+		expect(cart.getNumberOfItems()).toEqual(1);
 	});
 });
